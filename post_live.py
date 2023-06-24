3ea4389f076924f42d594bde2ae98ecf73c3e7cd
gchristodo <gchristodouloumath@gmail.com>
2023-06-24 09:35:52 +0300

    Added the post_live.py

---
+++ post_live.py
@@ -0,0 +1,19 @@
+import requests
+import json
+from settings import settings
+
+
+url = "https://project-udacity.onrender.com/inference"
+
+# Get the sample
+sample = settings["sample"]
+
+data = json.dumps(sample)
+
+# Post to API
+response = requests.post(url, data=data)
+
+# Display Response
+print("Status", response.status_code)
+print("Response:")
+print(response.json())
